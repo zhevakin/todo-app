847174e79c0a63482de6fa7ec07685173ccd15e9
Ivan Zhevakin <ixvakin@gmail.com>
2022-06-23 21:19:16 +0300

    users api

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import useAxios from 'axios-hooks'
 import Button from '../components/Button'
+
+const Error = styled.div`
+  color: red;
+`
 
 const SubmitButton = styled(Button)`
   font-size: 32px;
@@ -12,6 +17,10 @@ function User() {
   const [input, setInput] = useState('')
   const { userId } = useParams()
   const navigate = useNavigate()
+
+  const [{ data, loading, error }, refetch] = useAxios(
+    `https://pokeapi.co/api/v2/pokemon/${userId}`
+  )
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -30,6 +39,14 @@ function User() {
         />
         <SubmitButton type="submit">Отправить</SubmitButton>
       </form>
+      {loading && <span>Загрузка...</span>}
+      {error && <Error>Ошибка</Error>}
+      {!error && data && (
+        <div>
+          <h3>{data.name}</h3>
+          <img src={data.sprites.front_default} alt="" />
+        </div>
+      )}
     </Container>
   )
 }
